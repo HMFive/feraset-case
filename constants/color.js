4b212745814e63cdd454c21090659cd55f0331ce
yusufciftci <yusufciftci@outlook.com.tr>
2025-05-12 10:52:01 +0300

    create colors

---
+++ constants/color.js
@@ -0,0 +1,9 @@
+export const colors = {
+  backgroundGradient1: "#101013",
+  backgroundGradient2: "#24125c",
+  blueGradient: "#2938DC",
+  purpleGradient: "#943DFF",
+  textColor: "#FAFAFA",
+  secondaryTextColor: "#71717A",
+  containerBackgroundColor: "rgba(39,39,42,0.79)",
+};
