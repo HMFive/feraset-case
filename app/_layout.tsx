e89273f2e51f2c0a7722e26c081b457977d85f8a
yusufciftci <yusufciftci@outlook.com.tr>
2025-05-12 10:52:10 +0300

    add layout settings

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,32 @@
 import { Stack } from "expo-router";
+import { LinearGradient } from "expo-linear-gradient";
+import { colors } from "@/constants/color";
+
+export const unstable_settings = {
+  initialRouteName: "index",
+};
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerBackground: () => (
+          <LinearGradient
+            colors={[colors.backgroundGradient1, colors.backgroundGradient2]} // Blue to purple
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1.2, y: 0 }}
+            style={{ flex: 1 }}
+          />
+        ),
+        headerTitleStyle: { color: colors.textColor, fontSize: 17 },
+        headerTitle: "AI Logo",
+      }}
+    >
+      <Stack.Screen name={"prompt/index"}></Stack.Screen>
+      <Stack.Screen
+        name={"output/index"}
+        options={{ headerTitle: "" }}
+      ></Stack.Screen>
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,11 @@
-import { Text, View } from "react-native";
+import { Redirect } from "expo-router";
+import { useFonts } from "expo-font";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
-}
+const Index = () => {
+  useFonts({
+    Manrope: require("../assets/fonts/Manrope-VariableFont_wght.ttf"),
+  });
+  return <Redirect href="/prompt" />;
+};
+
+export default Index;
